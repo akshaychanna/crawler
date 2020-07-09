0712e820f297a8da67b1788dc5835bab3f097b85
akshay channa <akshaychanna77@gmail.com>
2020-07-09 16:14:38 +0530

    modified test file

--- test.py
+++ test.py
@@ -1,16 +1,24 @@
-list = [11,22,33]
-len = len(list)
-print(f"lenth of list is {len}")
-my_list = []
-print(my_list)
-for x in range(1,len+1):
-    obj = {}
-    obj['name'] = 'akshay'
-    obj['age'] = 24
-    my_list.append(obj)
+import requests
+from bs4 import BeautifulSoup
+import pdb
 
-# print(my_list)
 
-for ind , data in enumerate(my_list):
-    print(ind , data)
-    print(type(ind))
+url = "http://jkmitchell.app.dealerslinkclassifieds.com/web_display/condition/New"
+print(f"url is : {url}")
+r = requests.get(url)
+soup = BeautifulSoup(r.content, 'html.parser')
+sample = []
+headings = []
+divs = soup.find_all("div", class_= "vehicle-padding")
+
+for data in divs:
+    sample.append(data.find_all('section'))
+
+for _listing in sample[0]:  
+    headings.append(_listing.find('h2',attrs={'title'}).get_text())
+        
+    
+pdb.set_trace()    
+    
+    
+
